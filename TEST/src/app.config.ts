e667692b4415b76b8a964a541abf63dad2689a2a
jiajiabaobei <125649514+jiajiabaobei@users.noreply.github.com>
2023-07-20 10:36:33 +0800

    新增内容

--- TEST/src/app.config.ts
+++ TEST/src/app.config.ts
@@ -1,6 +1,8 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/detail/index',
+    'pages/user/index'
   ],
   window: {
     backgroundTextStyle: 'light',
---
+++ TEST/src/pages/user/index.tsx
@@ -0,0 +1,12 @@
+import { View } from '@tarojs/components'
+import React, {Component} from 'react'
+
+export default class index extends Component {
+    render(){
+        return(
+            <View>
+                
+            </View>
+        )
+    }
+} 
